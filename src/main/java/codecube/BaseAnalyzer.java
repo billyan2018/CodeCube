0c0fa524828c650f2ad2566ba28bca7d83fce894
Bill <55395638+billyan2018@users.noreply.github.com>
2021-08-06 14:12:55 +0800

    Update BaseAnalyzer.java

--- src/main/java/codecube/BaseAnalyzer.java
+++ src/main/java/codecube/BaseAnalyzer.java
@@ -24,6 +24,7 @@ abstract class BaseAnalyzer {
 
     private LanguagePlugin newLanguagePlugin() {
         try {
+            String notUsedString = "not used";
             String inputFileExtension = InputFileExtensions.fromLanguageCode(language());
             return new LanguagePlugin(findPluginFile().toUri().toURL(), inputFileExtension);
         } catch (IOException e) {
